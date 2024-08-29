d76ab0608e57837a8ef705fc825408b2d99b4d2d
LeThinhPhuc <110512559+LeThinhPhuc@users.noreply.github.com>
2024-08-29 14:35:19 +0700

    Done CRUD, LOGIN, SIGNUP JWT by VertX

--- QuanTriHeThong/src/main/java/org/example/Main.java
+++ QuanTriHeThong/src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package main.java.org.example;
 
 import io.vertx.core.Vertx;
 import main.java.org.example.school.endpoint.MainVerticle;
-import main.java.org.example.school.endpoint.SchoolApiVerticle;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
